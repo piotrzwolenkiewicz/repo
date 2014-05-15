fdee7df4926561fe7960834831d7c1f73e7a4e40
piotrzwolenkiewicz <piotr.zwolenkiewicz@gmail.com>
2014-05-15 17:02:20 +0200

    rozgrzebana.....

--- kalk.py
+++ kalk.py
@@ -1,6 +1,5 @@
-def dodawanie():
-    a=int(input("1. liczba"))
-    b=int(input("2. liczba"))
-    print(a+b)
-    
-dodawanie()
+def dodawanie(a,b):
+    return a+b    
+a=int(input("1. liczba"))
+b=int(input("2. liczba"))
+print(dodawanie(a,b))
